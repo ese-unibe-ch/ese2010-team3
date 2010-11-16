3efd84ed27cfde5923b28765ad0100315ec967d5
Sascha Schwaerzler <s.schwaerzler@students.unibe.ch>
2010-11-16 17:19:58 +0100

    deleteconfirmation fixup 1

--- qa/app/controllers/Secured.java
+++ qa/app/controllers/Secured.java
@@ -19,12 +19,14 @@ public class Secured extends Controller {
 	public static void newQuestion(@Required String content, String tags) {
 		if (!Validation.hasErrors()) {
 			User user = Session.get().currentUser();
-			Question question = Database.get().questions().add(user, content);
-			question.setTagString(tags);
-			user.startObserving(question);
-			question.setTagString(tags);
-			flash.success("Good luck for getting a reasonable answer!");
-			Application.question(question.id());
+			if (!user.isBlocked()) {
+				Question question = Database.get().questions().add(user, content);
+				question.setTagString(tags);
+				user.startObserving(question);
+				question.setTagString(tags);
+				flash.success("Good luck for getting a reasonable answer!");
+				Application.question(question.id());
+			}
 		} else {
 			flash.error("Please don't ask empty questions.");
 			Application.index();
@@ -34,9 +36,9 @@ public class Secured extends Controller {
 	public static void newAnswer(int questionId, @Required String content) {
 		Question question = Database.get().questions().get(questionId);
 		if (!Validation.hasErrors() && question != null) {
-			User thisUser = Session.get().currentUser();
-			if (!question.isLocked()) {
-				question.answer(thisUser, content);
+			User user = Session.get().currentUser();
+			if (!question.isLocked() && !user.isBlocked()) {
+				question.answer(user, content);
 				flash.success("Thanks for posting an answer.");
 			}
 		} else {
@@ -48,10 +50,9 @@ public class Secured extends Controller {
 	public static void newCommentQuestion(int questionId,
 			@Required String content) {
 		Question question = Database.get().questions().get(questionId);
-
-		if (!Validation.hasErrors() && question != null && !question.isLocked()) {
-			User thisUser = Session.get().currentUser();
-			question.comment(thisUser, content);
+		User user = Session.get().currentUser();
+		if (!Validation.hasErrors() && question != null && !question.isLocked() && !user.isBlocked()) {
+			question.comment(user, content);
 			flash.success("May your comment be helpful in clarifying the question!");
 			Application.question(questionId);
 		}
@@ -61,8 +62,9 @@ public class Secured extends Controller {
 			@Required String content) {
 		Question question = Database.get().questions().get(questionId);
 		Answer answer = question != null ? question.getAnswer(answerId) : null;
-		if (!Validation.hasErrors() && answer != null && !question.isLocked()) {
-			answer.comment(Session.get().currentUser(), content);
+		User user = Session.get().currentUser();
+		if (!Validation.hasErrors() && answer != null && !question.isLocked() && !user.isBlocked()) {
+			answer.comment(user, content);
 			flash.success("May your comment be helpful in clarifying the answer!");
 			Application.question(questionId);
 		}
