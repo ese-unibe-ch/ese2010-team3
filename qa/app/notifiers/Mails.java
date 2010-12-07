004fb5a6c509f9c770d30b938f640bc32dbc3726
sbuenzli <sbuenzli@student.unibe.ch>
2010-12-07 21:32:50 +0100

    even simpler with sendAndWait

--- qa/app/notifiers/Mails.java
+++ qa/app/notifiers/Mails.java
@@ -1,8 +1,5 @@
 package notifiers;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
 import models.User;
 import play.exceptions.MailException;
 import play.mvc.Mailer;
@@ -14,18 +11,6 @@ public class Mails extends Mailer {
 		addRecipient(user.getEmail());
 		setFrom("ajopi <noreply@arcadeweb.ch>");
 		String key = user.getConfirmKey();
-		Future<Boolean> isSent = send(user, key);
-
-		// busy wait until the e-mail has been sent
-		while (!isSent.isDone())
-			;
-		try {
-			// check if sending has been successful
-			return isSent.get();
-		} catch (InterruptedException e) {
-			return false;
-		} catch (ExecutionException e) {
-			return false;
-		}
+		return sendAndWait(user, key);
 	}
 }
